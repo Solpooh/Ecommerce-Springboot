8639e99d64d0a2f9de61d823f815466ee686c02c
Solpooh <hsw9420@naver.com>
2024-02-27 15:27:53 +0900

    [feat] 로그인 성공 시 이동할 index.html 구현

--- Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -24,6 +25,11 @@ public class LoginController {
     @GetMapping("/login")
     public String loginForm() {
         return "login";
+    }
+
+    @RequestMapping("/index")
+    public String home() {
+        return "index";
     }
 
     @GetMapping("/register")

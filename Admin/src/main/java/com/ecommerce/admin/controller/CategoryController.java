6c57ec075d568afd77a1bf0daa7028b2fd41a580
Solpooh <hsw9420@naver.com>
2024-02-28 12:44:25 +0900

    [feat] Category add 메서드 구현

--- Admin/src/main/java/com/ecommerce/admin/controller/CategoryController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/CategoryController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,6 +22,18 @@ public class CategoryController {
         model.addAttribute("categories", categories);
         model.addAttribute("size", categories.size());
         model.addAttribute("title", "Category");
+        model.addAttribute("categoryNew", new Category());
         return "categories";
+    }
+    @PostMapping("/add-category")
+    public String add(@ModelAttribute("categoryNew") Category category, RedirectAttributes attributes) {
+        try {
+            categoryService.save(category);
+            attributes.addFlashAttribute("success", "추가 성공");
+        } catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "추가 실패");
+        }
+        return "redirect:/categories";
     }
 }

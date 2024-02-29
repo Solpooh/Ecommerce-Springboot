65c7810a959a0cff1642617818f93eb754fd04a6
Solpooh <hsw9420@naver.com>
2024-02-29 18:18:10 +0900

    [feat] 이미지 등록 + 새 상품 등록 마무리

--- Admin/src/main/java/com/ecommerce/admin/controller/ProductController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/ProductController.java
@@ -8,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.util.List;
@@ -33,10 +38,27 @@ public class ProductController {
         return "products";
     }
     @GetMapping("/add-product")
-    public String addProductForm(Model model) {
+    public String addProductForm(Model model, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login";
+        }
         List<Category> categories = categoryService.findAllByActivated();
         model.addAttribute("product", new ProductDto());
         model.addAttribute("categories", categories);
         return "add-product";
+    }
+    @PostMapping("/save-product")
+    public String saveProduct(@ModelAttribute("product")ProductDto productDto,
+                              @RequestParam("imageProduct")MultipartFile imageProduct,
+                              RedirectAttributes attributes) {
+
+        try {
+            productService.save(imageProduct, productDto);
+            attributes.addFlashAttribute("success", "등록 완료!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("error", "등록 실패");
+        }
+        return "redirect:/products";
     }
 }

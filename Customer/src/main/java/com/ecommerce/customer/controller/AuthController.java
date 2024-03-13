1293ba3763d56b1740951633de3618c9b49eb3c8
Solpooh <hsw9420@naver.com>
2024-03-13 09:54:36 +0900

    [refactor] 비밀번호 저장 해시알고리즘 적용

--- Customer/src/main/java/com/ecommerce/customer/controller/AuthController.java
+++ Customer/src/main/java/com/ecommerce/customer/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.ecommerce.library.model.Customer;
 import com.ecommerce.library.service.CustomerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +16,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class AuthController {
     @Autowired
     private CustomerService customerService;
+
+    // 비밀번호 해시 알고리즘
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
@@ -33,28 +38,28 @@ public class AuthController {
                                   Model model) {
         try {
             if (result.hasErrors()) {
-                model.addAttribute("customerDto", new CustomerDto());
+                model.addAttribute("customerDto", customerDto);
                 return "register";
             }
             Customer customer = customerService.findByUsername(customerDto.getUsername());
             if(customer != null){
                 model.addAttribute("username", "이미 등록된 id");
-                model.addAttribute("customerDto", new CustomerDto());
+                model.addAttribute("customerDto", customerDto);
                 return "register";
             }
             if(customerDto.getPassword().equals(customerDto.getRepeatPassword())){
-//                customerDto.setPassword(passwordEncoder.encode(customerDto.getPassword()));
+                customerDto.setPassword(passwordEncoder.encode(customerDto.getPassword()));
                 customerService.save(customerDto);
                 model.addAttribute("success", "회원가입 성공");
                 return "register";
             }else{
                 model.addAttribute("password", "비밀번호가 일치하지 않습니다");
-                model.addAttribute("customerDto", new CustomerDto());
+                model.addAttribute("customerDto", customerDto);
                 return "register";
             }
         }catch (Exception e){
             model.addAttribute("error", "서버 에러 발생");
-            model.addAttribute("customerDto", new CustomerDto());
+            model.addAttribute("customerDto", customerDto);
         }
         return "register";
     }

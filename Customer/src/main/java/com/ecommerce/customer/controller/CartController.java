1fcd60b387c2a78753582bedfab2a78609992a50
Solpooh <hsw9420@naver.com>
2024-03-15 17:04:36 +0900

    [refactor] CartController Cart 메서드 수정

--- Customer/src/main/java/com/ecommerce/customer/controller/CartController.java
+++ Customer/src/main/java/com/ecommerce/customer/controller/CartController.java
@@ -1,5 +1,10 @@
 package com.ecommerce.customer.controller;
 
+import com.ecommerce.library.model.Customer;
+import com.ecommerce.library.model.ShoppingCart;
+import com.ecommerce.library.service.CustomerService;
+import com.ecommerce.library.service.ShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +13,24 @@ import java.security.Principal;
 
 @Controller
 public class CartController {
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ShoppingCartService cartService;
     @GetMapping("/cart")
     public String cart(Model model, Principal principal) {
         if (principal == null) {
             return "redirect:/login";
         }
+        String username = principal.getName();
+        Customer customer = customerService.findByUsername(username);
+        ShoppingCart shoppingCart = customer.getShoppingCart();
+        if (shoppingCart == null) {
+            model.addAttribute("check", "장바구니가 비었습니다");
+        }
+        model.addAttribute("shoppingCart", shoppingCart);
+
         return "cart";
     }
 }

9138cd8148b5e394c7e08c17910509cdb34070c3
Solpooh <hsw9420@naver.com>
2024-03-20 16:09:08 +0900

    [feat] 주문 3계층 구현

--- Library/src/main/java/com/ecommerce/library/model/OrderDetail.java
+++ Library/src/main/java/com/ecommerce/library/model/OrderDetail.java
@@ -16,14 +16,14 @@ public class OrderDetail {
     @Column(name = "order_detail_id")
     private Long id;
     private int quantity;
-    private double totalPrice;
+//    private double totalPrice;
     private double unitPrice;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", referencedColumnName = "product_id")
-    private Product product;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "order_id", referencedColumnName = "order_id")
     private Order order;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", referencedColumnName = "product_id")
+    private Product product;
+
 }
